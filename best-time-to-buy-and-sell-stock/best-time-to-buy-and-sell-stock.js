51a48ffdfdac2909cf2a346d2134bfdb49eeef2d
Gilbert Bui <gilbertbuix57@gmail.com>
2021-02-15 16:05:33 -0800

    Time: 104 ms (50.02%), Space: 49.1 MB (10.67%) - LeetHub

--- best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
+++ best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -3,13 +3,10 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let maxProfit = 0, minPrice = Infinity
-    for(let i = 0; i < prices.length; i++){
-        if (minPrice > prices[i]){
-            minPrice = prices[i]
-        }else if (maxProfit < prices[i] - minPrice) {
-            maxProfit = prices[i] - minPrice
-        }
+    let maxProfit = 0, current = 0
+    for(let i = 1; i < prices.length; i++){
+        current = Math.max(0, current += prices[i] - prices[i-1])
+        maxProfit = Math.max(current, maxProfit)
     }
     return maxProfit
 };
