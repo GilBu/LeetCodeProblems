cbfc5bf491f642749aaa09e557abd485c2f9e5b8
Gilbert Bui <gilbertbuix57@gmail.com>
2021-02-22 19:08:05 -0800

    Time: 140 ms (16.01%), Space: 39 MB (92.48%) - LeetHub

--- trapping-rain-water/trapping-rain-water.js
+++ trapping-rain-water/trapping-rain-water.js
@@ -3,31 +3,13 @@
  * @return {number}
  */
 var trap = function(height) {
-    let left = 0, leftIdx = 0, rightIdx = 0, right = 0, water = 0, fill = 0
-    for(let n = 0; n < height.length; n++) {
-        let elev = height[n]
-        
-        if(elev > 0 && left === 0){
-            left = elev
-            leftIdx = n
-        }
-        
-        if(elev < left){
-            fill += left - elev
-        }
-        
-        if(elev >= left){
-            water += fill
-            fill = 0
-            left = elev
-            leftIdx = n
-        }
-        
-        if(fill > 0 && n === height.length - 1 && leftIdx < height.length - 1){
-            n = leftIdx
-            left -= 1
-            fill = 0
-        }
+    let left = 0, right = height.length - 1, high = 0, low = 0, result = 0
+    
+    while (left < right){
+        low = height[height[left] < height[right] ? left++ : right--]
+        high = Math.max(high, low)
+        result += high - low
     }
-    return water
+    
+    return result
 };
