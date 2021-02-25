dea24c6453ea484ff1867304985ba6a14ca9fc03
Gilbert Bui <gilbertbuix57@gmail.com>
2021-02-24 16:48:21 -0800

    Time: 80 ms (71.29%), Space: 38.7 MB (70.41%) - LeetHub

--- valid-parentheses/valid-parentheses.js
+++ valid-parentheses/valid-parentheses.js
@@ -3,36 +3,18 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    let per = 0, cur = 0, brac = 0, queue = []
-    for(let i = 0; i < s.length; i++) {
-        if(s[i] === "("){
-            per++
-            queue.push(s[i])
-        }
-        else if(s[i] === ")" && (per == 0 || queue[queue.length - 1] != "(")) return false
-        else if(s[i] === ")" && per > 0){
-            per--
-            queue.pop()
-        }
-        else if(s[i] === "{"){
-            cur++
-            queue.push(s[i])
-        }
-        else if(s[i] === "}" && (cur == 0 || queue[queue.length - 1] != "{")) return false
-        else if(s[i] === "}" && cur > 0){
-            cur--
-            queue.pop()
-        }
-        else if(s[i] === "["){
-            brac++
-            queue.push(s[i])
-        }
-        else if(s[i] === "]" && (brac == 0 || queue[queue.length - 1] != "[")) return false
-        else if(s[i] === "]" && brac > 0){
-            brac--
-            queue.pop()
-        }
+    let stack = []
+    let paren = {
+        '(' : ')',
+        '{' : '}',
+        '[' : ']'
+        
     }
-    if(queue.length === 0) return true
-    else return false
+    for(let i = 0; i < s.length; i++){
+        if(paren[s[i]]) stack.push(paren[s[i]])
+        else if(stack[stack.length - 1] === s[i]) stack.pop()
+        else if(stack[stack.length - 1] != s[i]) return false
+    }
+    console.log(stack)
+    return stack.length === 0
 };
