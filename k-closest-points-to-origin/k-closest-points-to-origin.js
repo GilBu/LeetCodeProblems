ee13f29acd2d197f2694d7decbd5c92ce7da8c29
Gilbert Bui <gilbertbuix57@gmail.com>
2021-03-16 14:45:14 -0700

    Time: 204 ms (60.55%), Space: 51.6 MB (53.38%) - LeetHub

--- k-closest-points-to-origin/k-closest-points-to-origin.js
+++ k-closest-points-to-origin/k-closest-points-to-origin.js
@@ -4,17 +4,7 @@
  * @return {number[][]}
  */
 var kClosest = function(points, k) {
-    let smallest = 0, farthest = 0, dists = []
-    for(let i = 0; i < points.length; i++) {
-        if(dists.length < k - 1) dists.push(points[i])
-        else if(dists.length === k - 1){
-            dists.push(points[i])
-            dists.sort((a, b) => Math.sqrt(a[0]**2 + a[1]**2) - Math.sqrt(b[0]**2 + b[1]**2))
-        }else{
-            dists.push(points[i])
-            dists.sort((a, b) => Math.sqrt(a[0]**2 + a[1]**2) - Math.sqrt(b[0]**2 + b[1]**2))
-            dists.pop()
-        }
-    }
+    points.sort((a, b) => Math.sqrt(a[0]**2 + a[1]**2) - Math.sqrt(b[0]**2 + b[1]**2))
+    let dists = points.slice(0,k)
     return dists
 };
