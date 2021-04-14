682e2d69bcdc3603622cedfac3a43b6ae6c0c292
Gilbert Bui <gilbertbuix57@gmail.com>
2021-04-13 19:19:57 -0700

    Time: 144 ms (85.00%), Space: 48.6 MB (84.36%) - LeetHub

---
+++ 3sum/3sum.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+    if(nums.length === 0 || (nums.length === 1 && nums[0] === 0)) return []
+    let result = []
+    nums.sort((a,b) => a - b)
+    for(let i = 0; i < nums.length - 2; i++){
+        if(nums[i] > 0) return result
+        
+        if(i > 0 && nums[i] == nums[i - 1]) continue
+        
+        for (let j = i + 1, k = nums.length - 1; j < k;){
+            if(nums[i] + nums[j] + nums[k] === 0){
+                result.push([nums[i], nums[j], nums[k]])
+                j++
+                k++
+                while (j < k && nums[j] == nums[j-1]) j++
+                while (j < k && nums[k] == nums[k+1]) k--
+            } else if(nums[i] + nums[j] + nums[k] > 0) {
+                k--
+            } else j++
+        }
+    }
+    return result
+};
