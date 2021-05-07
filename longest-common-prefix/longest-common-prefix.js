3e44a017f00029daf85d3e4c13e971dac9e9efc0
Gilbert Bui <gilbertbuix57@gmail.com>
2021-05-06 21:08:05 -0700

    Time: 92 ms (35.64%), Space: 40.2 MB (32.98%) - LeetHub

---
+++ longest-common-prefix/longest-common-prefix.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    let prefix = ''
+    for(let i = 0; i < strs[0].length; i++){
+        for(let j = 1; j < strs.length; j++){
+            if(strs[j][i] != strs[j - 1][i]) return prefix
+        }
+        prefix += strs[0][i]
+    }
+    
+    return prefix
+};
