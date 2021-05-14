c70100d2b9c648c7c30b0e8edf971a5b250e07d5
Gilbert Bui <gilbertbuix57@gmail.com>
2021-05-13 20:54:04 -0700

    Time: 80 ms (39.31%), Space: 38.3 MB (85.46%) - LeetHub

---
+++ letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+var letterCombinations = function(digits) {
+    if (digits == null || digits.length === 0) return []
+
+  const map = {
+    2: 'abc',
+    3: 'def',
+    4: 'ghi',
+    5: 'jkl',
+    6: 'mno',
+    7: 'pqrs',
+    8: 'tuv',
+    9: 'wxyz'
+  }
+
+  const result = [];
+  const combi = (i, str) => {
+    if (i === digits.length) {
+      result.push(str)
+      return
+    }
+
+    for (const char of map[digits[i]]) {
+      combi(i + 1, str + char)
+    }
+  }
+
+  combi(0, '')
+  return result
+};
