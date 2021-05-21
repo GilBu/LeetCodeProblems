cbc5fdf4d8cebdf7a22c41e461ca93da626d9936
Gilbert Bui <gilbertbuix57@gmail.com>
2021-05-20 22:34:44 -0700

    Time: 100 ms (43.35%), Space: 47.4 MB (16.86%) - LeetHub

---
+++ remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+var removeDuplicates = function(s, k) {
+    const stack = []
+    
+    for(let char of s){
+        if(stack.length && stack[stack.length-1][0] === char){
+            stack[stack.length-1][1] += 1
+            if(stack[stack.length - 1][1] === k) stack.pop()
+        }else stack.push([char, 1])
+    }
+    
+    let result = ''
+    
+    for(let [char, count] of stack){
+        result += char.repeat(count)
+    }
+    
+    return result
+};
