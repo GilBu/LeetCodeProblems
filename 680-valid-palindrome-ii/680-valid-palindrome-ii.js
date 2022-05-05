060b12cc0177c956ff044c3db8e334f199a6f503
Gilbert Bui <gilbertbuix57@gmail.com>
2022-05-04 18:14:46 -0700

    Time: 83 ms (84.72%), Space: 48.3 MB (51.37%) - LeetHub

---
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var validPalindrome = function(s) {
+    let hasError = false
+    let i = 0
+    let j = s.length - 1
+    while (i < j) {
+        if(s[i] != s[j]) {
+            return isPalindrome(s, i, j - 1) || isPalindrome(s, i + 1, j)
+        }
+        i++
+        j--
+    }
+    return true
+};
+
+var isPalindrome = function(s, i, j) {
+    while (i < j) {
+        if(s[i] != s[j]) {
+            return false
+        }
+        i++
+        j--
+    }
+    return true
+}
