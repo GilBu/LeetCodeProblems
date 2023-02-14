cdd28ebd2196c810b27fbdf1f3a97e03a5b331bd
Gilbert Bui <gilbertbuix57@gmail.com>
2023-02-13 21:30:40 -0800

    Time: 69 ms (43.05%), Space: 41.6 MB (67.19%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.js
+++ 0278-first-bad-version/0278-first-bad-version.js
@@ -22,16 +22,14 @@ var solution = function(isBadVersion) {
         let right = n
         
         
-        while(left <= right){
+        while(left < right){
             let mid = Math.floor((right - left) / 2) + left
-            let currVer = isBadVersion(mid)
-            if(currVer && !isBadVersion(mid - 1)){
-                return mid
-            }else if(!currVer){
+            if(isBadVersion(mid)){
+                right = mid
+            }else{
                 left = mid + 1
-            }else {
-                right = mid - 1
             }
         }
+        return left
     };
 };
