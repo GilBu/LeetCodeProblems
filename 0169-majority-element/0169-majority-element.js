4ec2dc7ce0411f96e0514fbc41061f9f9d21a303
Gilbert Bui <gilbertbuix57@gmail.com>
2023-02-15 20:37:44 -0800

    Time: 71 ms (70.93%), Space: 44.8 MB (24.84%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -2,15 +2,21 @@
  * @param {number[]} nums
  * @return {number}
  */
+// var majorityElement = function(nums) {
+//     let elementCount = new Map()
+//     let majority = nums[0]
+    
+//     for(num of nums){
+//         if(!elementCount.has(num)) elementCount.set(num, 0)
+//         elementCount.set(num, elementCount.get(num) + 1)
+//         if(elementCount.get(majority) < elementCount.get(num)) majority = num
+//     }
+    
+//     return majority
+// };
+
 var majorityElement = function(nums) {
-    let elementCount = new Map()
-    let majority = nums[0]
+    nums.sort((a, b) => a - b)
     
-    for(num of nums){
-        if(!elementCount.has(num)) elementCount.set(num, 0)
-        elementCount.set(num, elementCount.get(num) + 1)
-        if(elementCount.get(majority) < elementCount.get(num)) majority = num
-    }
-    
-    return majority
+    return nums[Math.floor(nums.length / 2)]
 };
