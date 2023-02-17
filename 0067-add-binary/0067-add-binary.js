454699d8d3b830bc3ec1b0149680ba91c28878cc
Gilbert Bui <gilbertbuix57@gmail.com>
2023-02-16 18:24:40 -0800

    Time: 168 ms (5.23%), Space: 49.4 MB (5.10%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.js
@@ -0,0 +1,40 @@
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+var addBinary = function(a, b) {
+    let lengthA = a.length - 1
+    let lengthB = b.length - 1
+    
+    if(lengthA < lengthB) return addBinary(b, a)
+    
+    let carry = 0
+    let result = ''
+    for(let i = lengthA; i >= 0; i--) {
+        if(a[i] === '1') carry++
+        if(lengthB >= 0 && b[lengthB] === '1') {
+            carry++
+        }
+        lengthB--
+        console.log(carry)
+        if(carry % 2 === 1) {
+            result = result.concat('1')
+        } else {
+            result = result.concat('0')
+        }
+        
+        carry = Math.floor(carry / 2)
+        console.log(result, carry)
+    }
+    
+    if (carry === 1) result = result.concat('1')
+    return result.split('').reverse().join('')
+};
+
+/*
+  111 1 1
+  110 1 0
+  100 0 1
+  000 0 0
+  */
