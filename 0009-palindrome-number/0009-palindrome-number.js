1a93b7e2796353c9230d653faddf3435c1319a44
Gilbert Bui <gilbertbuix57@gmail.com>
2023-02-22 10:19:31 -0800

    Time: 292 ms (14.13%), Space: 61.2 MB (5.11%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -3,14 +3,27 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    let numStringArray = x.toString().split('')
-    let left = 0
-    let right = numStringArray.length - 1
-    while(left < right){
-        if(numStringArray[left] !== numStringArray[right]) return false
-        left++
-        right--
-    }
+    if(x < 0 || (x % 10 === 0 && x !== 0)) return false
     
-    return true
+    let flippedNum = 0
+    
+    while(x > flippedNum){
+        flippedNum = flippedNum * 10 + x % 10
+        x = Math.floor(x / 10)
+    }
+    console.log(flippedNum, x)
+    return flippedNum === x || Math.floor(flippedNum/10) === x
 };
+// var isPalindrome = function(x) {
+//     let numStringArray = x.toString().split('')
+//     let left = 0
+//     let right = numStringArray.length - 1
+//     while(left < right){
+//         if(numStringArray[left] !== numStringArray[right]) return false
+//         left++
+//         right--
+//     }
+    
+//     return true
+// };
+
