63de95287ae397be552ee4b420027c11122c2f81
Gilbert Bui <gilbertbuix57@gmail.com>
2023-02-22 10:07:06 -0800

    Time: 71 ms (77.01%), Space: 44.8 MB (25.76%) - LeetHub

--- 0268-missing-number/0268-missing-number.js
+++ 0268-missing-number/0268-missing-number.js
@@ -3,15 +3,12 @@
  * @return {number}
  */
 var missingNumber = function(nums) {
-    let numSet = new Set()
-    
-    for(let i = 0; i <= nums.length; i++){
-        numSet.add(i)
-    }
+    let length = nums.length
+    let sum = (length + 1) * length / 2
     
     for(num of nums){
-        numSet.delete(num)
+        sum -= num
     }
     
-    return numSet.values().next().value
+    return sum
 };
