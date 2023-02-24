a6cb75ec98d7cc970b7debd306afa81eac50c57e
Gilbert Bui <gilbertbuix57@gmail.com>
2023-02-23 19:39:05 -0800

    Time: 77 ms (96.66%), Space: 49.5 MB (19.12%) - LeetHub

---
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} subRoot
+ * @return {boolean}
+ */
+var isSubtree = function(root, subRoot) {
+    if (!root) return !subRoot
+    return isEqual(root, subRoot) || isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot)
+    
+};
+
+let isEqual = (node1, node2) => {
+    if (!node1 || !node2) return !node1 && !node2
+    if(node1.val !== node2.val) return false
+    return isEqual(node1.left, node2.left) && isEqual(node1.right, node2.right)
+}
